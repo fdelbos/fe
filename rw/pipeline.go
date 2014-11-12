56c43ed4e13c3a79196c1e1c5276e0957034ba35
Frederic Delbos <fred.delbos@gmail.com>
2014-11-12 16:20:03 +0100

    pipelines are now cancelable on error

--- rw/pipeline.go
+++ rw/pipeline.go
@@ -8,12 +8,16 @@ package rw
 
 import (
 	"errors"
-	"fmt"
 	"io"
+	"sync"
 )
 
 type Pipeline struct {
-	lastReader io.Reader
+	lastReader io.ReadCloser
+	errorChain []chan error
+	cancelChain []chan struct{}
+	Errors []error
+	size int
 }
 
 type EncodingPipeline struct {
@@ -26,58 +30,133 @@ type DecodingPipeline struct {
 	Input    Inputer
 }
 
-func (p *Pipeline) Exec(w io.Writer) error {
-	done := make(chan int)
+func (p *Pipeline) mergeErrors(chans []chan error) <- chan error {
+	var wg sync.WaitGroup
+	out := make(chan error, 1)
+	
+	output := func(c <-chan error) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+    }
+    wg.Add(p.size)
+    for _, c := range chans {
+        go output(c)
+    }
 
 	go func() {
-		_, err := io.Copy(w, p.lastReader)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-		done <- 1
-	}()
-	<-done
-
-	return nil
+        wg.Wait()
+        close(out)
+    }()
+    return out
 }
 
-func NewEncoding(encoders []Encoder, r io.Reader, d *Data) (*Pipeline, error) {
-	if len(encoders) == 0 {
+func (p *Pipeline) stopAll() {
+	for _, c := range(p.cancelChain) {
+		close(c)
+	}
+}
+
+func (p *Pipeline) Exec(w io.Writer) error {
+	defer p.stopAll()
+
+	errCh := make(chan error)
+	go func() {
+		_, err := io.Copy(w, p.lastReader)
+		errCh <- err
+		close(errCh)
+	}()	
+	errCopy := <- errCh
+	if errCopy != nil {
+		return errCopy
+	}
+
+	p.Errors = make([]error, p.size)
+	i := 0
+	var firstError error
+	for n := range p.mergeErrors(p.errorChain) {
+		p.Errors[i] = n
+		if firstError == nil && p.Errors[i] != nil {
+			firstError = p.Errors[i]
+			break
+		}
+		i += 1
+	}
+	return firstError
+}
+
+func newPipeline(r io.ReadCloser, size int) (*Pipeline, error){
+	if size == 0 {
 		return nil, errors.New("pipeline: A pipeline should have at lease 1 operation")
 	}
 	p := &Pipeline{
 		lastReader: r,
-	}
-	for _, e := range encoders {
-		nextReader, writer := io.Pipe()
-
-		go func(e Encoder, r io.Reader, w *io.PipeWriter, d *Data) {
-			e.Encode(r, w, d)
-			w.Close()
-		}(e, p.lastReader, writer, d)
-
-		p.lastReader = nextReader
+		errorChain: make([]chan error, size),
+		cancelChain: make([]chan struct{}, size),
+		size: size,
 	}
 	return p, nil
 }
 
-func NewDecoding(decoders []Decoder, r io.Reader, d *Data) (*Pipeline, error) {
-	if len(decoders) == 0 {
-		return nil, errors.New("pipeline: A pipeline should have at lease 1 operation")
+func NewEncoding(encoders []Encoder, r io.ReadCloser, d *Data) (*Pipeline, error) {
+	p, err := newPipeline(r, len(encoders))
+	if err != nil {
+		return nil, err
 	}
-	p := &Pipeline{
-		lastReader: r,
-	}
-	for _, e := range decoders {
+	for i, e := range encoders {
+		errCh := make(chan error, 1)
+		cancelCh := make(chan struct{}, 1)
 		nextReader, writer := io.Pipe()
 
-		go func(e Decoder, r io.Reader, w *io.PipeWriter, d *Data) {
-			e.Decode(r, w, d)
-			w.Close()
+		go func(e Encoder, r io.ReadCloser, w *io.PipeWriter, d *Data) {
+			defer w.Close()
+			defer close(errCh)
+
+			go func() {
+				select {
+				case <- cancelCh:
+					r.Close()
+				}
+			}()
+
+			errCh <- e.Encode(r, w, d)
 		}(e, p.lastReader, writer, d)
 
 		p.lastReader = nextReader
+		p.errorChain[i] = errCh
+		p.cancelChain[i] = cancelCh
+	}
+	return p, nil
+}
+
+func NewDecoding(decoders []Decoder, r io.ReadCloser, d *Data) (*Pipeline, error) {
+	p, err := newPipeline(r, len(decoders))
+	if err != nil {
+		return nil, err
+	}
+	for i, e := range decoders {
+		errCh := make(chan error, 1)
+		cancelCh := make(chan struct{}, 1)
+		nextReader, writer := io.Pipe()
+
+		go func(e Decoder, r io.ReadCloser, w *io.PipeWriter, d *Data) {
+			defer w.Close()
+			defer close(errCh)
+
+			go func() {
+				select {
+				case <- cancelCh:
+					r.Close()
+				}
+			}()
+
+			errCh <- e.Decode(r, w, d)
+		}(e, p.lastReader, writer, d)
+
+		p.lastReader = nextReader
+		p.errorChain[i] = errCh
+		p.cancelChain[i] = cancelCh
 	}
 	return p, nil
 }
--- rw/pipeline_test.go
+++ rw/pipeline_test.go
@@ -3,6 +3,7 @@ package rw_test
 import (
 	"bytes"
 	"crypto/rand"
+	"io"
 	. "github.com/fdelbos/fe/rw"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -10,7 +11,7 @@ import (
 
 var _ = Describe("Pipeline", func() {
 
-	testBin := make([]byte, 1<<16)
+	testBin := make([]byte, 1<<18)
 	rand.Read(testBin)
 
 	It("should execute a simple pipeline", func() {
@@ -21,9 +22,14 @@ var _ = Describe("Pipeline", func() {
 			Name: "cat",
 		}
 		cat.Init()
+		r, w := io.Pipe()
+		go func() {
+			w.Write(testBin)
+			w.Close()
+		}()
 		p1, err := NewEncoding(
 			[]Encoder{cat, cat, cat},
-			bytes.NewReader(testBin),
+			r,
 			data)
 		Ω(err).To(BeNil())
 		Ω(p1.Exec(out1)).To(BeNil())
@@ -35,9 +41,15 @@ var _ = Describe("Pipeline", func() {
 			Name: "zip",
 		}
 		zip.Init()
+		
+		r, w = io.Pipe()
+		go func() {
+			w.Write(testBin)
+			w.Close()
+		}()
 		p2, err := NewEncoding(
 			[]Encoder{zip},
-			bytes.NewReader(testBin),
+			r,
 			data)
 		Ω(err).To(BeNil())
 		Ω(p2.Exec(out2)).To(BeNil())
@@ -49,12 +61,94 @@ var _ = Describe("Pipeline", func() {
 			Name: "unzip",
 		}
 		unzip.Init()
+		r, w = io.Pipe()
+		go func() {
+			w.Write(out2.Bytes())
+			w.Close()
+		}()
 		p3, err := NewEncoding(
 			[]Encoder{unzip, zip, unzip},
-			bytes.NewReader(out2.Bytes()),
+			r,
 			data)
 		Ω(err).To(BeNil())
 		Ω(p3.Exec(out3)).To(BeNil())
 		Ω(bytes.Equal(out3.Bytes(), testBin)).To(BeTrue())
 	})
+
+	It("should crash with only one command", func() {
+		out := new(bytes.Buffer)
+		data := NewData()
+		crash := &Shell{
+			Cmd:  "exit 1",
+			Name: "crash",
+		}
+		crash.Init()
+		r, w := io.Pipe()
+		go func() {
+			w.Write(testBin)
+			w.Close()
+		}()
+		p, err := NewEncoding(
+			[]Encoder{crash},
+			r,
+			data)
+		Ω(err).To(BeNil())
+		Ω(p.Exec(out)).ToNot(BeNil())
+		Ω(len(out.Bytes())).To(Equal(0))
+	})
+
+	It("should crash with pending commands after", func() {
+		out := new(bytes.Buffer)
+		data := NewData()
+		cat := &Shell{
+			Cmd:  "cat",
+			Name: "cat",
+		}
+		cat.Init()
+		crash := &Shell{
+			Cmd:  "exit 1",
+			Name: "crash",
+		}
+		crash.Init()
+		r, w := io.Pipe()
+		go func() {
+			w.Write(testBin)
+			w.Close()
+		}()
+		p, err := NewEncoding(
+			[]Encoder{crash, cat, cat, cat},
+			r,
+			data)
+		Ω(err).To(BeNil())
+		Ω(p.Exec(out)).ToNot(BeNil())
+		Ω(len(out.Bytes())).To(Equal(0))
+	})
+
+	It("should crash with pending encoders before", func() {
+		out := new(bytes.Buffer)
+		data := NewData()
+		cat := &Shell{
+			Cmd:  "cat",
+			Name: "cat",
+		}
+		cat.Init()
+		crash := &Shell{
+			Cmd:  "exit 1",
+			Name: "crash",
+		}
+		crash.Init()
+		r, w := io.Pipe()
+		go func() {
+			w.Write(testBin)
+			w.Close()
+		}()
+		p, err := NewEncoding(
+			[]Encoder{cat, cat, cat, crash, cat, cat},
+			r,
+			data)
+		Ω(err).To(BeNil())
+		Ω(p.Exec(out)).ToNot(BeNil())
+		Ω(len(out.Bytes())).To(Equal(0))
+	})
+	
 })
